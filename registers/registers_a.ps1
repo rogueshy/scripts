9f9662a9da8c7cdf7359dde37973223ed744c8d4
Alexander <rogueshy@derpymail.org>
2017-03-13 17:31:58 +0300

    updates
    For clients in domain:
    * Reworked hardware detection
    * Reworked IP and MAC lines
    * Reworked output format, now in one line for JSON-alike (can't use JSON
    convertion because of Powershell v2.0 not supporting this cmdlet).
    
    Both clients:
    * Out-File changed to [System.IO.File] because of the new line at the
    end of file.

--- registers/registers_a.ps1
+++ registers/registers_a.ps1
@@ -1,9 +1,13 @@
-﻿#модель материнки
+# output path & filename:
+$out_file = "%your_path_to_folder%"+$env:computername+".txt"
+
+# motherboard stuff
 $System = GWMI Win32_OperatingSystem
 $CPU = GWMI Win32_Processor
-$Ram = $System.TotalVisibleMemorySize / 1000000
+$Ram = $System.TotalVisibleMemorySize*1024/1GB
 $Ram = "{0:N0}" -f $Ram
-# Блок для определения ID TeamViewer'а  (только для 9-ой версии), с обработкой его отсутствия
+<# Checking if TeamViewer v9 is present and getting it's ID
+    If it's not - write 000000000 instead of ID  #>
 If ($System.OSArchitecture -like "*64*") {
     $TV = Get-ItemProperty HKLM:\Software\Wow6432Node\TeamViewer\Version9\ ClientID -ErrorAction SilentlyContinue }
     Else {
@@ -12,28 +16,50 @@ If ($System.OSArchitecture -like "*64*") {
 If ($TV.ClientID -notmatch "[0-9]") { $TV_ID = "000000000" }
     else { $TV_ID = $TV.ClientID }
 
-#Блок для определения IP-адреса
-$ip = (ipconfig | Select-String IPv4) -split ":" | Select-String -notmatch ". ." -ErrorAction SilentlyContinue
+# Network info: IP and MAC.
+$network = GWMI Win32_NetworkAdapterConfiguration -filter 'IPEnabled=True'| Where {$_.MACAddress} | select -Last 1 IPAddress,MACAddress
+$ip = $network.IPAddress | Select-String -NotMatch ":"
+$ip = $ip -split " "
+$mac = $network.MACAddress
 
-
-#Ноутбук или нет, и модель материнки
+# Check SystemType.
+# If it's 1 or 3 than it's Desktop computer, if it's 2 than it's laptop, and on all other cases consider this as a server machine
 $HW = GWMI Win32_ComputerSystem
 $PC_Type = $HW.PCSystemType
 If ($PC_Type -eq 2) { 
-    #$Manuf = $HW.Manufacturer
+    $Manuf = ($HW.Manufacturer) -split " " | Select-Object -First 1
     $Model = $HW.Model
-    $out_hw = "N, $Model"
+    $type = "N"
+    $out_hw = "$Manuf $Model"
     }
-Else { 
+Elseif (($PC_Type -eq 1) -or ($PC_Type -eq 3)) { 
     $MB = GWMI Win32_Baseboard
-    #$MB_Manuf = $MB.Manufacturer
+    $MB_Manuf = ($MB.Manufacturer) -split " " | Select-Object -First 1
     $MB_Model = $MB.Product
-    $out_hw = "D, $MB_Model" }
+    $type = "D"
+    $out_hw = "$MB_Manuf $MB_Model" }
+Else {
+    $Manuf = ($HW.Manufacturer) -split " " | Select-Object -First 1
+    $Model = $HW.Product
+    $type = "S"
+    $out_hw = "$Manuf $Model"
+    }
 
-#Для всего остального
+#CPU & OS
 $CPName = $CPU.Name
 $OS = $System.Caption
 $OS = $OS + " " + $System.OSArchitecture
+#isDomain
+If ($HW.PartOfDomain -eq $True ) {
+    $Dom = "True"
+    }
+Else { $Dom = "False" }
 
-#Вывод
-$env:COMPUTERNAME, $CPName, $Ram, $OS, $TV_ID, $ip, $out_hw, $env:username | Out-File -FilePath "%folder_path\$env:COMPUTERNAME.txt" -Encoding UTF8
+# there's an extra space in system variable $username for some reason, off with it
+$user = $env:username -split " " | Select -First 1
+
+#output format
+$out = '{"Domain":"'+$Dom+'","CPU":"'+$CPName+'","RAM":"'+$Ram+'","OS":"'+$OS+'","TV":'+$TV_ID+',"IP":"'+$ip+'","MAC":"'+$mac+'","Type":"'+$type+'","HW":"'+$out_hw+'","User":"'+$user+'"}'
+
+# Writing all we've got into a file without new line
+[System.IO.File]::WriteAllText("$out_file","$out",[text.encoding]::UTF8)
--- registers/registers_b.ps1
+++ registers/registers_b.ps1
@@ -5,5 +5,4 @@ $3 = $user.SamAccountName
 $4 = $user.Name
 $5 = $user.EmailAddress
 #$6 = $env:CLIENTNAME
-
-$1, $2, $3, $4, $5 | Out-File -FilePath "%out_folder%\$env:CLIENTNAME.txt" -Encoding UTF8
+[System.IO.File]::WriteAllText("%out_folder%\$env:CLIENTNAME.txt","$1`r`n$2`r`n$3`r`n$4`r`n$5",[Text.Encoding]::UTF8)
