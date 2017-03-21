b8fe81ab2cf8804dd95385565c4c78dd7175019e
Alexander <rogueshy@derpymail.org>
2017-03-21 10:56:53 +0300

    Dusting
    TV tweak for correct JSON parsing and removed quotes around RAM value.

--- registers/registers_a.ps1
+++ registers/registers_a.ps1
@@ -1,5 +1,5 @@
 # output path & filename:
-$out_file = "%your_path_to_folder%"+$env:computername+".txt"
+$out_file = "folder"+$env:computername+".txt"
 
 # motherboard stuff
 $System = GWMI Win32_OperatingSystem
@@ -7,13 +7,14 @@ $CPU = GWMI Win32_Processor
 $Ram = $System.TotalVisibleMemorySize*1024/1GB
 $Ram = "{0:N0}" -f $Ram
 <# Checking if TeamViewer v9 is present and getting it's ID
-    If it's not - write 000000000 instead of ID  #>
+    If it's not - write a single 0 instead of ID
+    changed from 000000000 for correct JSON parsing  #>
 If ($System.OSArchitecture -like "*64*") {
     $TV = Get-ItemProperty HKLM:\Software\Wow6432Node\TeamViewer\Version9\ ClientID -ErrorAction SilentlyContinue }
     Else {
         $TV = Get-ItemProperty HKLM:\Software\TeamViewer\Version9 ClientID -ErrorAction SilentlyContinue }
 
-If ($TV.ClientID -notmatch "[0-9]") { $TV_ID = "000000000" }
+If ($TV.ClientID -notmatch "[0-9]") { $TV_ID = "0" }
     else { $TV_ID = $TV.ClientID }
 
 # Network info: IP and MAC.
@@ -59,7 +60,7 @@ Else { $Dom = "False" }
 $user = $env:username -split " " | Select -First 1
 
 #output format
-$out = '{"Domain":"'+$Dom+'","CPU":"'+$CPName+'","RAM":"'+$Ram+'","OS":"'+$OS+'","TV":'+$TV_ID+',"IP":"'+$ip+'","MAC":"'+$mac+'","Type":"'+$type+'","HW":"'+$out_hw+'","User":"'+$user+'"}'
+$out = '{"Domain":"'+$Dom+'","CPU":"'+$CPName+'","RAM":'+$Ram+',"OS":"'+$OS+'","TV":'+$TV_ID+',"IP":"'+$ip+'","MAC":"'+$mac+'","Type":"'+$type+'","HW":"'+$out_hw+'","User":"'+$user+'"}'
 
 # Writing all we've got into a file without new line
 [System.IO.File]::WriteAllText("$out_file","$out",[text.encoding]::UTF8)
